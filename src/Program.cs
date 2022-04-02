50044ad629300e8c9d534662c078a2459e09eedd
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2022-04-01 17:20:02 -0700

    General changes
    Rules and greeting addition and changed function name that changed

--- src/Program.cs
+++ src/Program.cs
@@ -1,22 +1,28 @@
-﻿using System;
+using System;
 
-namespace SnakeGame
+namespace TicTacToe
 {
     class Program
     {
         static void Main(string[] args)
         {
             GenerateBoard game = new GenerateBoard();
-            rules();
-            game.setBoard();
+            greeting();
+
+            game.initBoard();
+            Console.WriteLine("Enter the x,y coordinates of where you want to place your peice");
+            game.addToBoard(0,0);
             game.showBoard();
+
+            
 
 
         }
         
-        static void rules()
+        static void greeting()
         {
-            Console.WriteLine("Rules: its tick tac toe lol");
+            Console.WriteLine("weclome to tic tac toe\n");
+            Console.WriteLine("Rules:\nPlayer 1 goes first\n");
         }
     }
 }
