189a44afe4289ab9b92b24481cbbe6107cc99372
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2022-04-25 16:35:02 -0700

    minimalist main

--- src/Program.cs
+++ src/Program.cs
@@ -2,27 +2,18 @@ using System;
 
 namespace TicTacToe
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            GenerateBoard game = new GenerateBoard();
-            greeting();
-
-            game.initBoard();
-            Console.WriteLine("Enter the x,y coordinates of where you want to place your peice");
-            game.addToBoard(0,0);
-            game.showBoard();
-
-            
-
-
-        }
+ 
+    class Program {
         
-        static void greeting()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("weclome to tic tac toe\n");
-            Console.WriteLine("Rules:\nPlayer 1 goes first\n");
+            
+            gameControl flowcontrol = new gameControl();
+
+            flowcontrol.controlHub();
         }
-    }
+
+
+    } 
 }
+
